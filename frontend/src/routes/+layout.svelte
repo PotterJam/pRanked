6256e68bdb960e4fa450847b98ee17e8b9d210b8
James Potter <jamesrpotter1@gmail.com>
2023-11-04 12:22:50 +0000

    Get it looking reasonable again

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+
+<slot />
+
+<style lang="postcss">
+    :global(body) {
+      @apply bg-darkcream pl-5 pr-5 pb-5;
+    }
+  </style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import leaderboard from '$lib/assets/leaderboard.svg'
+    import graph from '$lib/assets/graph.svg'
+    import cog from '$lib/assets/cog.svg'
+</script>
+
+<div class="flex flex-row justify-center h-20 w-auto">
+    <button class="bg-navbut w-60 mr-5 sm:mr-10 rounded-b border-l border-r border-b-4 border-black"
+    on:click={() => {}}>
+        <img src={leaderboard} class="m-auto h-12 w-12" alt="Leaderboard icon" />
+    </button>
+    <button class="bg-navbut w-60 mr-5 sm:mr-10 rounded-b border-l border-r border-b-4 border-black"
+    on:click={() => {}}>
+        <img src={graph} class="m-auto h-12 w-12" alt="Graph icon" />
+    </button>
+    <button class="bg-navbut w-60 rounded-b border-l border-r border-b-4 border-black"
+            on:click={() => {}}>
+        <img src={cog} class="m-auto h-12 w-12" alt="Settings icon" />
+    </button>
+</div>
