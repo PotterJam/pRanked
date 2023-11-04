8bd7b68a70f276ea6575e91d6b2ab59767b48c72
James Potter <jamesrpotter1@gmail.com>
2023-11-04 18:31:24 +0000

    Add a reset, again button

--- frontend/src/components/GamePlayerSelector.svelte
+++ frontend/src/components/GamePlayerSelector.svelte
@@ -33,11 +33,20 @@
 		submitGame(selectedPlayer1, selectedPlayer2, draw);
 	};
 
+	const again = () => {
+		complete = false;
+		onSubmit = false;
+		selectedPlayer1 = '';
+		selectedPlayer2 = '';
+		draw = false;
+	};
+
 </script>
 
 <div class="flex flex-col w-auto justify-end p-5">
 	{#if complete}
 	<div class="p-10 flex" in:scale><img src={check} class="m-auto h-12 w-12" alt="Check mark" /></div>
+	<button in:slide={{delay: 700}} class="btn text-xl bg-stone-100 w-32 h-12 mb-0 mx-3 mt-2" on:click={again}>Again?</button>
 	{:else if onSubmit}
 		<div class="p-10 flex" in:fade><Spinny /></div>
 	{:else if selectedPlayer1 == '' || selectedPlayer2 == ''}
@@ -50,9 +59,9 @@
 		<option value="busa">Busa</option>
 	</select>
 	{:else}
-		<div class="text-center text-2xl p-5 pt-3 font-medium">Pick the winner</div>
-		<button in:slide class="btn bg-emerald-400 text-xl w-60 h-14 m-3" on:click={topWon}>{selectedPlayer1}</button>
-		<button in:slide class="btn bg-emerald-300 text-xl w-60 h-14 m-3 mt-2" on:click={bottomWon}>{selectedPlayer2}</button>
+		<div class="text-center text-2xl p-5 pt-3 font-medium">Who won?</div>
+		<button in:slide class="btn bg-cyan-50 text-xl w-60 h-14 m-3" on:click={topWon}>{selectedPlayer1}</button>
+		<button in:slide class="btn bg-cyan-50 text-xl w-60 h-14 m-3 mt-2" on:click={bottomWon}>{selectedPlayer2}</button>
 
 		<div class="mb-5 text-center flex flex-col justify-center">
 			<div class="flex flex-row flex-wrap justify-center">
